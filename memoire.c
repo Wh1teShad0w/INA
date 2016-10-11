0ce8bbff9988e470e884492c203d8ab31af74726
Ange SAADJIO <sanix000@yahoo.fr>
2016-10-11 13:59:59 +0200

    Update memoire.c

--- memoire.c
+++ memoire.c
@@ -1,1 +1,10 @@
 
+#include <stdio.h>
+#include <memoire.h>
+
+int main (void)       /*  fonction principale               */
+{
+  printf ("Memoire Launch\n") ;
+  Launch(OPT_LINKUY);
+  return 1 ;
+}
